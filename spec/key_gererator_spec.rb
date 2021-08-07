d54dec0ef328b1b72b51cde63b3a50504a6b8e00
Miriam Aceves <miriammerari02@gmail.com>
2021-08-07 18:32:26 -0400

    Write test for key method

--- spec/key_gererator_spec.rb
+++ spec/key_gererator_spec.rb
@@ -1,11 +1,17 @@
-require './spec_helper'
 require './lib/key_generator'
+require './spec_helper'
 require 'pry'
 
 RSpec.describe KeyGenerator do
   it 'exists' do
-    key = KeyGenerator.new
+    new_key = KeyGenerator.new
 
-    expect(key).to be_a(KeyGenerator)
+    expect(new_key).to be_a(KeyGenerator)
+  end
+
+  it 'can generate random key' do
+
+    expect(KeyGenerator.key.length).to eq(5)
+    expect(KeyGenerator.key.class).to eq(String)
   end
 end
